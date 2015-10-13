ee94ce56c55cb4c4ce68f62893e8dc790dd9dd61
Aditya Menon <aditya.menon@io-media.com>
2015-10-13 06:38:29 +0530

    Tracker manual code practice

--- meteor-explained.js
+++ meteor-explained.js
@@ -1,70 +1,42 @@
 if (Meteor.isClient) {
-    uname = null;
-    unameDep = new Tracker.Dependency;
+  var bank = new ReactiveDict();
+  bank.set('savings', 50);
+  bank.set('checking', 10);
+  bank.set('checksAllowed', true);
 
-    somethingElse = null;
-    seDep = new Tracker.Dependency;
+  Tracker.autorun(function() {
+    if (bank.get('checksAllowed')) {
+      console.log('Go ahead and write some checks!');
+    } else {
+      console.log('No money available, checks suspended!');
+    }
+  });
 
-    Tracker.autorun(function () {
-      unameDep.depend();
-      if (uname) {
-        console.log("Your uname: ".concat(uname))
+  // disable checkWriting when there isn't money
+  Tracker.autorun(function() {
+    console.log("There is $" + bank.get("checking") + " in your checking account.");
+
+    // afterFlush because we want to allow overdraft protection a chance
+    Tracker.afterFlush(function() {
+      if (bank.get('checking') < 0) {
+        console.log('No more checks for you!');
+        bank.set('checksAllowed', false);
       }
     });
+  });
 
-    Tracker.autorun(function () {
-      seDep.depend();
-      if (somethingElse) {
-        console.log("something else val: " + somethingElse);
-      }
-    })
+  writeACheck = function(amount) {
+    if (bank.get('checksAllowed')) {
+      bank.set('checking', bank.get('checking') - amount);
+    }
+  }
 
-    var TodoColl    = new Mongo.Collection(null),
-        alwaysTrue  = function () {return true};
-
-    TodoColl.allow({
-      insert: alwaysTrue,
-      update: alwaysTrue,
-      remove: alwaysTrue
-    });
-
-    Template.addTodo.events({
-      "click #add-todo": function (ev) {
-        ev.preventDefault();
-
-        TodoColl.insert({
-          createdAt: new Date(),
-          text: $("#todo-text").val(),
-          done: false
-        }, function () {
-          $("#todo-text").val("");
-        });
-      }
-    });
-
-    Template.todoList.helpers({
-      "todosList": function () { return TodoColl.find(); },
-      "checkedState": function () {
-        return TodoColl.findOne({_id: this._id}).done ? "checked" : "";
-      }
-    });
-
-    Template.todoList.events({
-      "change .todo-item-check": function (ev) {
-        var status = $(ev.target).is(':checked');
-        TodoColl.update({_id: this._id}, {$set: {done: status}}, function (e, n) {
-          if (!e) {
-            $(ev.target).parent().css('text-decoration', status ? 'line-through' : 'none');
-          }
-        });
-      },
-      "click #delete-done": function (ev) {
-        TodoColl.find({"done": true}).forEach(function (todo) {
-            TodoColl.remove({_id: todo._id});
-          });
-        }
-    });
-}
-
-if (Meteor.isServer) {
+  // overdraft protection
+  Tracker.autorun(function() {
+    if (bank.get('checking') < 0 && bank.get('savings') >= 25) {
+      bank.set('checking', bank.get('checking') + 25);
+      bank.set('savings', bank.get('savings') - 25);
+      console.log('Check writing privilege restored. Checking: ' + bank.get('checking') + '. Savings: ' + bank.get('savings'));
+    }
+  });
 }
